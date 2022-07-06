66996147975bda713b3a973fb1e5c5b66c2e002a
João Matos <joaomarcosm.silva@outlook.com>
2022-07-06 14:59:27 -0300

    add function reset password

--- Server/src/app/controllers/authController.js
+++ Server/src/app/controllers/authController.js
@@ -92,6 +92,35 @@ router.post('/forgot_password', async (req, res) => {
     } catch (err) {
         res.status(400).send({ error: 'Erro on forgot password, try again' });
     }
-})
+});
+
+router.post('/reset_password', async (req, res) => {
+    const { email, token, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email })
+            .select('+passwordResetToken passwordResetExpires');
+
+        if (!user)
+            return res.status(400).send({ error: 'User not found '});
+        
+        if (token !== user.passwordResetToken)
+            return res.status(400).send({ error: 'Token invalid' });
+
+        const now = new Date();
+
+        if (now > user.passwordResetExpires)
+            return res.status(400).send({ error: 'Token expired, generate a new one' });
+
+        user.password = password;
+
+        await user.save();
+        
+        res.send();
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Token invalid' });
+    }
+});
 
 module.exports = app => app.use('/auth', router);
